b446b0a31974b0ddf33d4b29646b97bd47d8ea91
SNOWIK1 <dimais444@mail.ru>
2023-04-15 13:19:13 +0700

    bashed at 13:18 15.04.2023

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using myMath;
+using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nEnter any numbers: ");
+        string str = Console.ReadLine();
+        easy.SumMult(str);
+    }
+}
---
+++ myMath/Class1.cs
@@ -0,0 +1,54 @@
+﻿namespace myMath
+{
+    public class easy
+    {
+
+        public static void SumMult(string str)
+        {
+
+            bool isPassed = true;
+            bool isFinished = false;
+
+                double sum = 0;
+                double mult = 1;
+
+                string[] arr = str.Split(' ');
+                double[] numbers = new double[arr.Length];
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+
+                    try
+                    {
+                        numbers[i] = double.Parse(arr[i]);
+
+                        sum += numbers[i];
+                        mult *= numbers[i];
+                        
+                        isPassed = true;
+                        
+                    }
+
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Incorrect input, try again...");
+                        isPassed = false;
+                        break;
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("Incorrect input, try again...");
+                        isPassed = false;
+                        break;
+
+                    }
+                }
+                if (isPassed)
+                {
+                    isFinished = true;
+                    Console.WriteLine($"Sum: {sum}\nMult: {mult}");
+                }
+
+        }
+    }
+}
